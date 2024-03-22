8062d72c434159bf99d315fa42535917cc6a8033
Nir <niryo@wix.com>
2024-03-22 21:41:18 +0200

    try reloading all spotify tabs

--- service-worker.js
+++ service-worker.js
@@ -6,19 +6,16 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
-chrome.alarms.create('checkPing', { periodInMinutes: 5 });
+chrome.alarms.create('checkPing', {periodInMinutes: 5});
 
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === 'checkPing') {
     const TWO_MINUTES = 2 * 60 * 1000;
     if (Date.now() - lastPing > TWO_MINUTES) {
-      chrome.tabs.query({ active: true }, (tabs) => {
-        const activeTab = tabs[0];
-        if (activeTab) {
-          if(activeTab.url.includes('spotify.com')) {
-            chrome.tabs.reload(tabs[0].id);
-          }
-        }
+      chrome.tabs.query({url: "*://*.spotify.com/*"}, function (tabs) {
+        tabs.forEach(function (tab) {
+          chrome.tabs.reload(tab.id);
+        });
       });
     }
   }
